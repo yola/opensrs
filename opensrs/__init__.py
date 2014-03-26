e12919f164dba489a952577b247b176847561a24
William Stewart <william@synthasite.com>
2014-03-26 12:17:08 +0200

    Replaced imports in __init__ with full path imports

--- opensrs/__init__.py
+++ opensrs/__init__.py
@@ -5,5 +5,5 @@ __doc__ = 'Client library for OpenSRS'
 __version__ = '0.0.1'
 __url__ = 'https://github.com/yola/opensrs'
 
-from opensrsapi import OpenSRS
-import errors
+from opensrs.opensrsapi import OpenSRS
+from opensrs import errors
