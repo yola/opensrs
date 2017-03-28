63c5d749569f8e8f6d1ea753623e0f5efc8001dd
James O'Donnell <james@synthasite.com>
2017-03-28 09:17:54 -0400

    bump version for missing transfer_id value bug fix

--- opensrs/__init__.py
+++ opensrs/__init__.py
@@ -2,7 +2,7 @@
 # namespacing cleaner.
 
 __doc__ = 'Client library for OpenSRS'
-__version__ = '3.0.0'
+__version__ = '3.0.1'
 __url__ = 'https://github.com/yola/opensrs'
 
 from opensrs.opensrsapi import OpenSRS
